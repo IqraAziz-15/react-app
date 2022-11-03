9abd9a4fcac051d6fd6ab940165d6e289866765f
IqraAziz-15 <iqra@wanclouds.net>
2022-11-03 20:43:49 +0500

    fix: add semantic release

---
+++ release.config.js
@@ -0,0 +1,5 @@
+module.exports={
+    branches: "master",   //we can have mutliple branches here too
+    repositoryUrl: "https://github.com/IqraAziz-15/react-app",
+    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
+}
